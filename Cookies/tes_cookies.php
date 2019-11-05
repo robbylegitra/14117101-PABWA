0c639ce135a3e7c5dd5fcdad315392b0beeb2a71
Robby Legitra Kurniawan <48901437+robbylegitra@users.noreply.github.com>
2019-11-05 14:44:39 +0700

    Update tes_cookies.php

--- Cookies/tes_cookies.php
+++ Cookies/tes_cookies.php
@@ -1,6 +1,11 @@
-<?php
- session_start();
- session_destroy();
-
- header('location:index.php');
- ?>
+<?php setcookie("test_cookie", "test", time() + 3600, '/'); ?>
+ <html>
+ <body>
+ <?php if(count($_COOKIE) > 0) {
+ echo "Cookies enabled.";
+ } else {
+ echo "Cookies disabled.";
+ } ?>
+ <!-- analisis disini -->
+ </body>
+ </html>
